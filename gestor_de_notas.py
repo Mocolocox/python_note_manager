f4919172e8916cb94b0d7fa27a3cd141901f8a72
Matias Palazzo <matiaspalazzo00900@gmail.com>
2026-08-04 16:15:17 -0300

    feat: agregar eliminación de notas

--- gestor_de_notas.py
+++ gestor_de_notas.py
@@ -6,6 +6,7 @@ while True:
     print("1. Agregar notas")
     print("2. Ver notas")
     print("3. Salir")
+    print("4. Eliminar Nota")
 
     usuario = int(input("Elegí una opción: "))
     if usuario == 1: 
@@ -18,5 +19,13 @@ while True:
     elif usuario == 3:
         print("fin del programa")
         break
+    elif usuario == 4:
+        if notas == []:
+            print("no hay elementos que eliminar, guarda algo primero")
+        else:
+            for posicion, nota in enumerate(notas):
+                print(posicion,nota)
+            opcion_usuario = int(input("¿que nota desea borrar?(elegir con numeros): "))
+            notas.pop(opcion_usuario)
     else: 
         print("Opcion invalida")
