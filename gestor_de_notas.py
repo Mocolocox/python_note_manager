520a8639992c9497b8101ca08aefcefb26ffe120
Matias Palazzo <matiaspalazzo00900@gmail.com>
2026-08-05 18:55:49 -0300

    fix: validar datos de entrada

--- gestor_de_notas.py
+++ gestor_de_notas.py
@@ -1,18 +1,21 @@
 #Gestor de notas
 notas = []
 
-while True: 
+while True:
     print("\n===== GESTOR DE NOTAS =====")
     print("1. Agregar notas")
     print("2. Ver notas")
     print("3. Salir")
     print("4. Eliminar Nota")
-
-    usuario = int(input("Elegí una opción: "))
-    if usuario == 1: 
+    try:
+        usuario = int(input("Elegí una opción: "))
+    except ValueError:
+        print("Opcion incorrecta, intenta con un numero")
+        break
+    if usuario == 1:
         notas.append(input("Escribí tu nota:"))
     elif usuario == 2:
-        if notas == []: 
+        if not notas:
             print("no hay notas, escribe algo primero")
         for nota in notas:
             print(nota)
@@ -20,12 +23,17 @@ while True:
         print("fin del programa")
         break
     elif usuario == 4:
-        if notas == []:
+        if not notas:
             print("no hay elementos que eliminar, guarda algo primero")
         else:
             for posicion, nota in enumerate(notas):
                 print(posicion,nota)
-            opcion_usuario = int(input("¿que nota desea borrar?(elegir con numeros): "))
-            notas.pop(opcion_usuario)
-    else: 
+            try:
+                opcion_usuario = int(input("¿que nota desea borrar?(elegir con numeros): "))
+                notas.pop(opcion_usuario)
+            except ValueError:
+                print("Opcion incorrecta, intenta con un numero")
+            except IndexError:
+                print("intenta con un numero dentro del rango")
+    else:
         print("Opcion invalida")
